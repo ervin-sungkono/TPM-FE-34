0e8df7887c22c7952ff2a2e6d293da9f62da6aa7
ervin-sungkono <ervinsung@gmail.com>
2023-01-06 11:41:51 +0700

    Add file pertemuan 5, form validation di pertemuan 1

--- Pertemuan 3/script.js
+++ Pertemuan 3/script.js
@@ -121,3 +121,13 @@ window.onscroll = function(){
     }
     prevYpos = Ypos;
 }
+
+const hamburgerButton = document.getElementById('hamburger-btn');
+const closeButton = document.getElementById('close-btn');
+
+hamburgerButton.addEventListener('click',toggleNav);
+closeButton.addEventListener('click',toggleNav)
+
+function toggleNav(){
+    document.getElementsByClassName('nav-menu')[0].classList.toggle('active');
+}
